e9d2e0c120d2070b4735958f1495e9eb7ab9b87f
unknown <nurmahannadmondol>
2025-02-27 13:15:43 +0430

    Created onlien shop client site

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-teal-500 text-center">
+        Online Shop
+      </h1>
+    </>
+  );
+}
+
+export default App;
